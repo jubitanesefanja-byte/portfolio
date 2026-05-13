de7b8aba88918b7faf7efd6afbcc815e0a5d28bd
jubitanesefanja-byte <jubitanesefanja@gmail.com>
2026-05-13 19:02:17 -0300

    app

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+
+/* SCROLL ANIMATIONS */
+const fadeElements = document.querySelectorAll('.fade-up');
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+fadeElements.forEach((element) => {
+    observer.observe(element);
+});
